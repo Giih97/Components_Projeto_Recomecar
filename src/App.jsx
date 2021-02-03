5f64bb061037c301d97bb5a8e1daef2792747271
Gilda Monteiro <67397737+Giih97@users.noreply.github.com>
2021-02-03 13:17:28 -0300

    excluindo componentes vazio do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Menu from "./Components/Menu";
-import Footer from "./Components/Footer";
-import Routes from "./Routes";
 import { BrowserRouter } from "react-router-dom";
 
 
@@ -10,15 +6,6 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Menu />
-        <main>
-            <Routes />
-        </main>
-          <div>
-            <footer>
-              <Footer />
-            </footer>
-          </div>
       </div>
     </BrowserRouter>
   );
